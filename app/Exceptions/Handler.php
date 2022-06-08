2fce335a0e77280166a185a55b4a4e32b0ac9a9c
Rosandymaulana <rosandy1118@gmail.com>
2022-06-08 21:19:57 +0700

    Praktikum 2 - Percobaan CRUD

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,18 +4,12 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
+// use Spatie\FlareClient\Http\Response;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
     /**
      * A list of the exception types that are not reported.
      *
@@ -46,5 +40,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, $exception)
+    {
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json(
+                ['message' => 'Not Found!'],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        return parent::render($request, $exception);
     }
 }
